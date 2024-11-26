aae8f71451073328d9c0d7179e832508eb580eb5
kuba <krzywdun@wp.pl>
2024-11-26 15:32:45 +0100

    little enchances

--- src/main/java/com/krzywdek19/student_diary/school/SchoolController.java
+++ src/main/java/com/krzywdek19/student_diary/school/SchoolController.java
@@ -5,12 +5,12 @@ import com.krzywdek19.student_diary.school.request.CreateSchoolRequest;
 import com.krzywdek19.student_diary.school.service.SchoolService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequestMapping("/api/v1/school")
 @RequiredArgsConstructor
 public class SchoolController {
